6ab4caf799d67da39b4330140a48140608152da2
Navan Sami <navan.sami@yahoo.com>
2022-10-16 00:30:51 +0400

    connected app to DB

--- config/db.js
+++ config/db.js
@@ -1,1 +1,13 @@
 const mongoose = require('mongoose')
+
+const DB = 'sales'
+const mongoURI = `mongodb+srv://admin:access12345@cluster0.t44pqfp.mongodb.net/${DB}`
+
+const connectDB = async () => {
+    await mongoose
+        .connect(mongoURI)
+        .then(data => console.log(`connected to database: ${DB}`))
+        .catch(err => console.error(err))
+}
+
+module.exports = connectDB
--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const server = require('./config/app')
+const connectDB = require('./config/db')
+
 const PORT = process.env.PORT || 8000
 const HOST = process.env.HOST || 'http://12.0.0.1'
+
+connectDB()
 
 server.listen(PORT, () => {
     console.log('--------------Server Stats------------------');
